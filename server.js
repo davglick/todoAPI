354f96897ec312a47356588a4f5a510509681692
Davin Glick <Dav.glick18@gmail.com>
2016-11-23 11:39:17 +1100

    Added post request for todos

--- server.js
+++ server.js
@@ -1,13 +1,57 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
+	
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 
 });
 
-app.listen(PORT, function () {
+// GET /todos
+app.get('/todos', function(req, res){
+    res.json(todos);
+});
 
+// GET /todos/:id
+
+app.get('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	} 
+});
+
+
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+
+	body.id = todoNextId++;
+ 
+    todos.push(body);
+
+	//console.log('description' + body.description);
+
+	res.json(body);
+
+});
+
+app.listen(PORT, function () {
 	console.log("Express Listening on prot: " + PORT)
 });
